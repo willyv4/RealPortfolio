a3113978e66030f0764a436720623c4ae55332d7
William Valadez <billy.valadez@icloud.com>
2022-09-18 21:02:02 -0400

    finishing up mobile friendly features

--- src/Compenants/MuiStyles.js
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-import useMediaQuery from '@mui/material/useMediaQuery';
-
-export default function SimpleMediaQuery() {
-  const matches = useMediaQuery('(max-width:600px)');
-
-  return <span>{`(max-width:600px) matches: ${matches}`}</span>;
-}
