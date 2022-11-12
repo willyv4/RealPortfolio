3599c1f4b51276a974ed05e9f8de116ce23aa8cd
William Valadez <billy.valadez@icloud.com>
2022-11-12 11:48:21 -0500

    .

--- src/Compenants/mainPage.js
+++ src/Compenants/mainPage.js
@@ -13,23 +13,26 @@ function mainPage() {
       <AboutMe />
       <div className="MainPage_Container">
         <div className="textContainer">
-          <h1 className="TEXT" id="projects">PROJECTS AND SKILLS</h1>
+          <h1 className="TEXT" id="projects">
+            PROJECTS AND SKILLS
+          </h1>
           <p className="projectandskills">
             I have honed a variety of skills in and outside the tech industry.
-            In the last 7 months I have created and contributed to many
+            In the last 10 months I have created and contributed to many
             projects. Here's some of the skills learned along the way... React,
-            JavaScript, CSS, HTML, NodeJS, APIs, Supabase, Firebase, Git, TypeScript, and
-            Wordpress.
+            JavaScript, CSS, HTML, NodeJS, APIs, Supabase, Firebase, Git,
+            TypeScript, and Wordpress.
             <br></br>
             <br></br>
-            Other notable skills: Management, Conflict resolution, Troubleshooting,
-            Building Relationships, Meeting Facilitation, Appointment Scheduling.
+            Other notable skills: Management, Conflict resolution,
+            Troubleshooting, Building Relationships, Meeting Facilitation,
+            Appointment Scheduling.
           </p>
         </div>
         <SwipeableTextMobileStepper />
       </div>
       <div className="SkillsWrapped">
-      <Skills />
+        <Skills />
       </div>
     </div>
   );
