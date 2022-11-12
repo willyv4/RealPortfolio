ff2283ec9e44f9464a0c2395ba4146e8d790c532
William Valadez <billy.valadez@icloud.com>
2022-11-12 01:09:34 -0500

    added resume

--- src/Compenants/AboutMe.js
+++ src/Compenants/AboutMe.js
@@ -17,13 +17,12 @@ const AboutMe = () => {
         development journey began when I realized that managing in the
         manufacturing industry wasn’t as fun or challenging as my newfound
         obsession; coding. From that point on I’ve made changes to invest my
-        personal time in fostering my tech skills. I got a less consuming job
-        and have been resourceful, self-motivated, and diligent as I have taught
-        myself web development. Over the past 7 months, I have become a
-        competent developer, but I have a lot to learn. I have the grit and
-        passion for it. My short-term goal is to land a web development
-        position. I'm eager to work with and learn from professionals. I'm
-        excited to put in the time and effort.
+        personal time in fostering my tech skills. I have been resourceful,
+        self-motivated, and diligent as I have taught myself web development.
+        Over the past 10 months, I have become a competent developer, but I have
+        a lot to learn. I have the grit and passion for it. My short-term goal
+        is to land a web development position. I'm eager to work with and learn
+        from professionals. I'm excited to put in the time and effort.
       </p>
       <div className="AboutMetriangle"></div>
     </div>
--- src/Compenants/ButtonGroup.js
+++ src/Compenants/ButtonGroup.js
@@ -43,7 +43,7 @@ const anchorbuttons = [
     </button>
   </a>,
   <a href="#aboutme" key="AboutMe">
-    <button className="buttons" style={{borderTopRightRadius: 5}}>
+    <button className="buttons" style={{ borderTopRightRadius: 5 }}>
       <svg
         className="icons"
         stroke="currentColor"
@@ -106,8 +106,12 @@ const anchorbuttons = [
       HANDSHAKE
     </button>
   </a>,
-  <a href="#resume" key="Resume" >
-    <button className="buttons" style={{borderBottomRightRadius: 5}} >
+  <a
+    href="https://www.canva.com/design/DAFRm60dcA4/d4x8644wY89LP89WFQAlfg/view?website#4"
+    target="blank"
+    key="Resume"
+  >
+    <button className="buttons" style={{ borderBottomRightRadius: 5 }}>
       {" "}
       <svg
         className="icons"
@@ -124,44 +128,6 @@ const anchorbuttons = [
       RESUME
     </button>
   </a>,
-  // <a href="https://mail.google.com/mail/u/0/#inbox" key="Email">
-  //   <button className="buttons">
-  //     {" "}
-  //     <svg
-  //       className="icons"
-  //       stroke="currentColor"
-  //       fill="currentColor"
-  //       stroke-width="0"
-  //       role="img"
-  //       viewBox="0 0 24 24"
-  //       height="2em"
-  //       width="2em"
-  //       xmlns="http://www.w3.org/2000/svg"
-  //     >
-  //       <title></title>
-  //       <path d="M24 4.5v15c0 .85-.65 1.5-1.5 1.5H21V7.387l-9 6.463-9-6.463V21H1.5C.649 21 0 20.35 0 19.5v-15c0-.425.162-.8.431-1.068C.7 3.16 1.076 3 1.5 3H2l10 7.25L22 3h.5c.425 0 .8.162 1.069.432.27.268.431.643.431 1.068z"></path>
-  //     </svg>
-  //     GMAIL
-  //   </button>
-  // </a>,
-  // <a href="https://github.com/willyv4" target="blank" key="GitHub">
-  //   <button className="buttons" style={{ borderBottomRightRadius: 5 }}>
-  //     {" "}
-  //     <svg
-  //       className="icons"
-  //       stroke="currentColor"
-  //       fill="currentColor"
-  //       stroke-width="0"
-  //       viewBox="0 0 1024 1024"
-  //       height="2em"
-  //       width="2em"
-  //       xmlns="http://www.w3.org/2000/svg"
-  //     >
-  //       <path d="M511.6 76.3C264.3 76.2 64 276.4 64 523.5 64 718.9 189.3 885 363.8 946c23.5 5.9 19.9-10.8 19.9-22.2v-77.5c-135.7 15.9-141.2-73.9-150.3-88.9C215 726 171.5 718 184.5 703c30.9-15.9 62.4 4 98.9 57.9 26.4 39.1 77.9 32.5 104 26 5.7-23.5 17.9-44.5 34.7-60.8-140.6-25.2-199.2-111-199.2-213 0-49.5 16.3-95 48.3-131.7-20.4-60.5 1.9-112.3 4.9-120 58.1-5.2 118.5 41.6 123.2 45.3 33-8.9 70.7-13.6 112.9-13.6 42.4 0 80.2 4.9 113.5 13.9 11.3-8.6 67.3-48.8 121.3-43.9 2.9 7.7 24.7 58.3 5.5 118 32.4 36.8 48.9 82.7 48.9 132.3 0 102.2-59 188.1-200 212.9a127.5 127.5 0 0 1 38.1 91v112.5c.8 9 0 17.9 15 17.9 177.1-59.7 304.6-227 304.6-424.1 0-247.2-200.4-447.3-447.5-447.3z"></path>
-  //     </svg>
-  //     GitHub
-  //   </button>
-  // </a>,
 ];
 
 export default function GroupOrientation() {
